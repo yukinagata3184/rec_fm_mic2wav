24193da47d03747f0f130e642c895fa2a8900c9e
yukinagata3184 <yuki.nagata3184@gmail.com>
2024-01-26 12:52:06 +0900

    #2 [Add] unit tests load inifile

---
+++ tests/unit/test_get_ini.py
@@ -0,0 +1,12 @@
+import pytest
+from src import get_ini
+
+def test_get_params_fm_ini_is_true():
+    assert get_ini.get_params_fm_ini(inifile_path="./param.ini", section_name="DEFAULT") == \
+           {'CHUNK': '1024', 'FORMAT': 'pyaudio.paInt16', 'CHANNELS': '1', 
+            'SAMP_RATE': '16000', 'RECORD_SECONDS': '10'}
+
+def test_get_params_fm_ini_is_false():
+    assert get_ini.get_params_fm_ini(inifile_path="./param.ini", section_name="DEFAULT") != \
+           {'CHUNK': '1024', 'FORMAT': 'pyaudio.paInt16', 'CHANNELS': '1', 
+            'SAMP_RATE': '16000', 'RECORD_SECONDS': '11'}
