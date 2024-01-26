696158f72cc32b332e5fab6fae7fbcf0b1943339
yukinagata3184 <yuki.nagata3184@gmail.com>
2024-01-26 16:12:09 +0900

    #8 [Add] rec_fm_mic2wav unit tests.

---
+++ tests/unit/test_fm_mic2wav.py
@@ -0,0 +1,13 @@
+import pytest
+import os
+import time
+from src import rec_fm_mic2wav
+
+def test_fm_mic2wav():
+    input_dict = {"CHUNK": 1024, "CHANNELS": 1, "SAMP_RATE": 16000, "RECORD_SECONDS": 3}
+    start = time.time()
+    rec_fm_mic2wav.rec_fm_mic2wav(param_dict=input_dict)
+    end = time.time()
+    assert (end - start) > input_dict["RECORD_SECONDS"]
+    assert os.path.isfile("./output.wav") == True
+    os.remove("./output.wav")
